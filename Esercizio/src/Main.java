fde1699201fc2275fa2ed55b8a045c7fb5e4eb02
FedericoIafolla <iafollafederico@gmail.com>
2024-10-02 16:48:45 +0200

    02/10

--- Esercizio/src/Main.java
+++ Esercizio/src/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         // Creazione dei prodotti con le date
         Product book1 = new Product(1L, "Il conte Draaaacula, minghia che pauuura", "Books", 120.0, LocalDate.of(2020, 12, 15));
-        Product book2 = new Product(2L, "Pdorrrr, figlio di Kmer", "Books", 105.0, LocalDate.of(2021, 1, 10));
+        Product book2 = new Product(2L, "Pdor, figlio di Kmer", "Books", 105.0, LocalDate.of(2021, 1, 10));
         Product book3 = new Product(3L, "La Subaru Baracca", "Books", 50.0, LocalDate.of(2021, 2, 5));
         Product book4 = new Product(4L, "La Subaru Baracca 2: modello banana", "Baby", 80.0, LocalDate.of(2021, 2, 20));
-        Product book5 = new Product(5L, "I medici", "Books", 150.0, LocalDate.of(2021, 3, 15));
+        Product book5 = new Product(5L, "Il dottor Helmut Alzhaimer", "Books", 150.0, LocalDate.of(2021, 3, 15));
         Product book6 = new Product(6L, "Che fa'? Mi guarda con quegli occhi da cernia? Mi vuole cernire il codice segreto?", "Books", 90.0, LocalDate.of(2021, 2, 25));
         Product book7 = new Product(7L, "La battaglia di Magenta", "Boys", 110.0, LocalDate.of(2021, 3, 1));
         Product book8 = new Product(8L, "I Maratoneti", "Books", 75.0, LocalDate.of(2021, 3, 5));
-        Product book9 = new Product(9L, "Sugar e le sue avventure", "Boys", 95.0, LocalDate.of(2021, 2, 28));
+        Product book9 = new Product(9L, "Sugar e le sue avventure poliziesche", "Boys", 95.0, LocalDate.of(2021, 2, 28));
 
         // Creazione dei clienti
         Customer customer1 = new Customer(1L, "Giacomino Poretti", 1);
